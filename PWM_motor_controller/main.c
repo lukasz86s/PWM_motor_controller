744d1b195f49b532015582c7a97c37db39e8dd56
lukasz86 <fetts@o2.pl>
2025-03-23 19:59:43 +0100

    Timer 2 set to CTC mode, PWM outputs created for pins PB0 - PB5. PWM timing accuracy tested

--- PWM_motor_controller/main.c
+++ PWM_motor_controller/main.c
@@ -6,21 +6,45 @@
  */ 
 
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+//#include <util/delay.h>
 #define F_CPU 16000000UL
 
-
+volatile uint8_t pwm_list[6] = {[0 ... 5] = 50} ;
+volatile uint8_t MAX_PWM = 3;
 int main(void)
 {	
-	int a = 0;
-	DDRB |= (1 << PB2); 
+	//set CTC mode on timer2
+	TCCR2A |= (1 << WGM21);		// CTC mode
+	TCCR2B |= (1 << CS21);		// preskaler 8
+	// set OCR2A to 199 (16Mzh / 8 / 200 = 0.1ms period)
+	OCR2A = 199;
+	// compare intterupt enable
+	TIMSK2 |= (1 << OCIE2A);
+	// set output on pins
+	DDRB |= ((1 << PB2) | (1 << PB1)| (1 << PB0));
+
+
+	// enable global interrupt 
+	sei(); 
     /* Replace with your application code */
     while (1) 
     {
-		PORTB ^= (1 << PB2); // Zmiana stanu pinu PB2
-		_delay_ms(400);
-		a++;
-		if(a > 10) a += 2;
+		pwm_list[2] = 85;
+		pwm_list[0] = 25;
+
+		
     }
+}
+
+ISR(TIMER2_COMPA_vect){
+	static uint8_t cnt ;
+	for(uint8_t i = 0; i < MAX_PWM; i++)
+	{
+		if(pwm_list[i] > cnt) PORTB |= (1 << i); else PORTB &= ~(1 << i);
+	}
+	
+	cnt ++;
+	if (cnt >= 100) cnt = 0;
 }
 
