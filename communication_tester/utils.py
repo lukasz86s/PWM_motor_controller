354f8330986f690e0ad56eb5b23ad8e15969e6e2
lukasz86 <fetts@o2.pl>
2025-03-31 11:28:10 +0200

    communication tester added calculate crc16 function

---
+++ communication_tester/utils.py
@@ -0,0 +1,12 @@
+import serial
+
+def calculate_crc16_MODBUS(data: bytes) -> int:
+    crc = 0xFFFF
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x0001:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc >>= 1
+    return crc
