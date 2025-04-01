830d2c75e4f0a80746d0faab95355080d2379b72
lukasz86 <fetts@o2.pl>
2025-04-01 12:53:15 +0200

    communication_tester -> implement create frame func, add tests for utils

--- communication_tester/utils.py
+++ communication_tester/utils.py
@@ -11,5 +11,24 @@ def calculate_crc16_MODBUS(data: bytes) -> int:
                 crc >>= 1
     return crc
 
-def create_frame(CMD: int, channels_to_set: int, channels_data: list):
-    pass
+FRAME_CONST_SIZE = 3
+FRAME_CRC_SIZE = 2
+def create_frame(cmd: int,  channels_data: list):
+    channels_data_size = len(channels_data)
+    assert channels_data_size % 2 == 0
+    channels_to_set_size = channels_data_size//2
+    data = []
+    data.append(0x55)
+    frame_size = channels_data_size + FRAME_CONST_SIZE + FRAME_CRC_SIZE
+    data.append(frame_size)
+    data.append(cmd)
+    data.append(channels_to_set_size)
+    [data.append(d) for d in channels_data]
+    crc = calculate_crc16_MODBUS(bytes(data[1:]))
+    high_byte = (crc >> 8) & 0xFF
+    low_byte = crc & 0xFF
+    data.append(high_byte)
+    data.append(low_byte) 
+    print(data)
+    print(crc)
+    return data   
